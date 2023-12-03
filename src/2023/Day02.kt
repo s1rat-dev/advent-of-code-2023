b4bb073b240a25d220bdebe981e0af874d39291f
Sirat Semih Cop <sirat.cop@d-teknoloji.com.tr>
2023-12-04 00:15:09 +0300

    day 2 solution

---
+++ src/2023/Day02.kt
@@ -0,0 +1,64 @@
+import java.io.File
+
+
+
+fun main() {
+    val path = "..."
+    val minimumValues = mutableMapOf(
+        "red" to 0,
+        "green" to 0,
+        "blue" to 0
+    )
+    val possibleGameValues = mutableMapOf(
+        "red" to 12,
+        "green" to 13,
+        "blue" to 14
+    )
+
+    fun String.withGameInfo(body: (List<String>) -> Unit) {
+        this.substringAfter(':').trim().split(';').forEach { game ->
+            game.split(',').forEach {
+                it.trim().split(" ").apply {
+                    body.invoke(this)
+                }
+            }
+        }
+    }
+
+    fun part1(): Int {
+        var point = 0
+
+        File(path).forEachLine { line ->
+            var eliminated = false
+            val gameId = line.substringBefore(':').takeLastWhile { it.isDigit() }.toInt()
+
+            line.withGameInfo {
+                if (it.first().toInt() > possibleGameValues[it.last()]!!) eliminated = true
+            }
+
+            if (!eliminated) point += gameId
+        }
+        return point
+    }
+
+    fun part2(): Int {
+        var point = 0
+
+        File(path).forEachLine { line ->
+
+            line.withGameInfo {
+                if (it.first().toInt() > minimumValues[it.last()]!!) {
+                    minimumValues[it.last()] = it.first().toInt()
+                }
+            }
+
+            point += minimumValues.values.reduce(Int::times)
+        }
+        return point
+    }
+
+
+    println(part1())
+    println(part2())
+
+}
