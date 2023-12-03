b07e41995edaff4220424c4061ed5c3fefe3e8b7
Sirat Semih Cop <sirat.cop@d-teknoloji.com.tr>
2023-12-04 00:15:14 +0300

    day 3 solution

---
+++ src/2023/Day03.kt
@@ -0,0 +1,71 @@
+import java.io.File
+
+fun main() {
+    val path = "..."
+    val specialChars = listOf('#', '*', '@', '+', '-', '%', '&', '/', '$', '=')
+    val multiply = '*'
+    val file = File(path).readLines()
+
+    fun getNumber(file: String, index: Int): Int {
+        val prefix = file.substring(0, index).takeLastWhile { it.isDigit() }
+        val suffix = file.substring(index).takeWhile { it.isDigit() }
+
+        return "$prefix$suffix".toInt()
+    }
+
+    fun part1(): Int {
+        var point = 0
+
+        file.forEachIndexed { index, line ->
+            line.forEachIndexed { ix, ch ->
+                if (ch in specialChars) {
+                    for (i in -1..1) {
+                        for (k in listOf(0, 1, -1)) {
+                            if (file[index + i][ix + k].isDigit()) {
+                                point += getNumber(file[index + i], ix + k)
+                                if (k == 0) break
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return point
+    }
+
+    fun part2(): Int {
+        var point = 0
+
+        file.forEachIndexed { index, line ->
+            line.forEachIndexed { ix, ch ->
+                if (ch == multiply) {
+                    var count = 0
+                    val nums = mutableListOf<Int>()
+
+                    for (i in -1..1) {
+                        for (k in listOf(0, 1, -1)) {
+                            if (file[index + i][ix + k].isDigit()) {
+                                nums.add(getNumber(file[index + i], ix + k))
+                                count += 1
+                                if (k == 0) break
+                            }
+                        }
+                        if (count > 2) break
+                    }
+
+                    if (count == 2) point += nums.reduce(Int::times)
+                }
+            }
+        }
+
+        return point
+    }
+
+
+    println(part1())
+    println(part2())
+
+}
+
+
